2b594a40b0d29438fa5c0e4a1e183cc06e8b861c
yabets <yabets328@gmail.com>
2024-09-03 05:45:19 +0300

    added signup route and middleware and a function to handle errors

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,32 @@
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    next(errorHandler(400, "please fill all thus fields"));
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  try {
+    await newUser.save();
+    res.json("SignUp successfully");
+  } catch (err) {
+    next(err);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 mongoose
@@ -14,8 +16,22 @@ mongoose
   });
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.listen(3000, () => {
   console.log("The server is running on port 3000!!!");
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(status).json({
+    success: false,
+    status,
+    message,
+  });
+});
