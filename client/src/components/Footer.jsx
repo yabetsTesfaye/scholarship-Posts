8706715826cf916f800031e8b9212d60c67d79f0
yabets <yabets328@gmail.com>
2024-09-05 01:25:54 +0300

    create the footer Component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,88 @@
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsLinkedin,
+  BsGithub,
+  BsDribbble,
+} from "react-icons/bs";
+
+export default function FooterComponent() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="w-full max-w-7xl mx-auto ">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                Scholar
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+            <div className="">
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="/https://www.100scholarships.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Above 100 Scholarships
+                </Footer.Link>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Blogs
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div className="">
+              <Footer.Title title="Follw Us" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="/https://www.github.com/yabetstesfaye"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href="#">Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div className="">
+              <Footer.Title title="Legal" />
+              <Footer.LinkGroup col>
+                <Footer.Link href="#">Privacy Policy</Footer.Link>
+                <Footer.Link href="#">Terms & Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className=" w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="JjScholarships"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center ">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="#" icon={BsLinkedin} />
+            <Footer.Icon href="#" icon={BsDribbble} />
+            <Footer.Icon href="#" icon={BsGithub} />
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
--- client/src/layouts/MainLayOut.jsx
+++ client/src/layouts/MainLayOut.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Headers from "../components/Header";
+import FooterComponent from "../components/Footer";
 export default function MainLayOut() {
   return (
     <div>
       <Headers />
       <Outlet />
+      <FooterComponent />
     </div>
   );
 }
